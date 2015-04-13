4b28d0abb41d2c10ebe4682b3641b323506c205e
ridger <jgjahuar@hotmail.com>
2015-04-13 09:26:57 -0500

    base del proyecto

---
+++ proyecto/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ridger
+ * Date: 13/04/2015
+ * Time: 9:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace proyecto
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
